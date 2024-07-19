b869956def1df826fe954750d49f27b02dc48b10
amandeepsingh1929 <as0632568@gmail.com>
2024-07-19 21:20:07 +0530

    Agrregation Pipline for channel profile.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -313,6 +313,77 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
 
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+  const {username} = req.params
+
+  if(!username?.trim()){
+    throw new ApiError(400 , 'Username is missing')
+  }
+  
+  const channel = await User.aggregate([
+    {
+      $match : {
+        username : username?.toLowerCase()
+      }
+    },
+    {
+      $lookup : {
+        from : "subscriptions",
+        localField : "_id",
+        foreignField : "channel", //Finding the subscriber of user with help of channel
+        as : "subscribers"
+      }
+    },
+    {
+      $lookup : {
+        from : "subscriptions",
+        localField : "_id",
+        foreignField : "subscriber", // Finding how many channels the user has subscribe 
+        as : "subscribedTo"
+      }
+    },
+    {
+      $addFields : {
+        subscriberCount : {
+          $size : "$subscribers"
+        },
+        channelSubscribedToCount : {
+          $size : "$subscribedTo"
+        },
+        isSubscribed : {
+          $cond : {
+            if : {$in : [req.user?._id, "$subscribers.subscriber"]},
+            then : true,
+            else : false
+          }
+        }
+      }
+    },
+    {
+      $project : {
+        fullname : 1,
+        username : 1,
+        email  : 1,
+        subscriberCount : 1,
+        channelSubscribedToCount : 1,
+        isSubscribed : 1,
+        avatar : 1,
+        coverImage : 1
+      }
+    }
+  ])
+ 
+  if(!channel?.length){
+    throw new ApiError(404 , "Channel does not exist")
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200 , channel[0],"User Channel fecthed successfully")
+  )
+
+})
 
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage}
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}
