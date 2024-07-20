50720745698518436aef809ab8640f0d37575f8b
amandeepsingh1929 <as0632568@gmail.com>
2024-07-21 01:49:30 +0530

    fixed current user response controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -232,8 +232,8 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 
 const getCurrentUser = asyncHandler(async(req,res)=>{
   return res.status(200).json(
-    200,
-    req.user, "Current user fetched successfully"
+    new ApiResponse (200,
+      req.user, "Current user fetched successfully")
   )
 })
 
